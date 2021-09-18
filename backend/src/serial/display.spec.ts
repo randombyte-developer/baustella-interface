5473a6840e6c62d1d42c8ae78318b9270a80711e
Sven Rahn <apps.randombyte@gmail.com>
2021-09-18 16:16:38 +0200

    First basic display

---
+++ backend/src/serial/display.spec.ts
@@ -0,0 +1,7 @@
+import { Display } from './display';
+
+describe('Display', () => {
+  it('should be defined', () => {
+    expect(new Display()).toBeDefined();
+  });
+});
--- backend/src/serial/serial.service.ts
+++ backend/src/serial/serial.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as SerialPort2 from 'serialport';
+import { Display } from './display';
 
 const Delimiter = require("@serialport/parser-delimiter");
 
@@ -7,6 +8,8 @@ const Delimiter = require("@serialport/parser-delimiter");
 export class SerialService {
 
     private port: SerialPort2;
+
+    private testNumber = 0;
 
     constructor() {
         this.port = new SerialPort2("/dev/ttyUSB0", { baudRate: 9600 }, function (error) {
@@ -16,8 +19,35 @@ export class SerialService {
         });
 
         const parser = this.port.pipe(new Delimiter({ delimiter: ";" }));
-        parser.on("data", function (data) {
+        parser.on("data", (data: Buffer) => {
             console.log("Data: " + data);
+
+            const match = data.toString().match("b(\\d):([01])");
+            if (!match) return;
+
+            this.onButton(parseInt(match[1]), parseInt(match[2]) === 1);
         });
+    }
+
+    private onButton(number: number, pressed: boolean) {
+        switch (number) {
+            case 1:
+                if (pressed) {
+                    this.testNumber++;
+                    this.updateDisplay();
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private updateDisplay() {
+        const a = Display.encodeNumber(this.testNumber);
+        let data = "d1:".split("").map(char => char.charCodeAt(0));
+        data.push(...a);
+        data.push(";".charCodeAt(0));
+        this.port.write(data)
     }
 }
